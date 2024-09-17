55cd6dd17ee6ed3faefb926a2f0588305622ec98
Joy-whale0321 <jingyuzhang21@m.fudan.edu.cn>
2024-09-18 00:29:56 +0800

    Update dphivsenergy.C

--- up2rcf/dphivsenergy.C
+++ up2rcf/dphivsenergy.C
@@ -13,19 +13,19 @@ void dphivsenergy() {
 
     // TFile *file = TFile::Open("/home/jingyu/sphenix/fromxvdong/PhotonEMC-main/output/TrackCalo_0_ana_truthcluster.root");
     // TTree *tree = (TTree*)file->Get("tree");
-    TChain *tree_chain("tree");
+    TChain *chain("tree");
     
     std::ifstream infile("list.txt");
     std::string filename;
 
     while (std::getline(infile, filename)) {
         if (!filename.empty()) {
-            chain.Add(filename.c_str());
+            chain->Add(filename.c_str());
             std::cout << "Added file: " << filename << std::endl;
         }
     }
     // 检查总共添加了多少事件
-    Long64_t nentries = chain.GetEntries();
+    Long64_t nentries = chain->GetEntries();
     std::cout << "Total number of entries in the chain: " << nentries << std::endl;
 
     TH2D *h_deltaphi_energy = new TH2D("h_deltaphi_energy", "h_deltaphi_energy", 10000, -0.5, 0.5, 12, -1, 11);
@@ -37,18 +37,18 @@ void dphivsenergy() {
     std::vector<double> *_truth_e             = nullptr;
 
     // 设置分支地址
-    tree_chain->SetBranchAddress("_emcal_phi", &_emcal_phi);
-    tree_chain->SetBranchAddress("_truth_phi", &_truth_phi);
-    tree_chain->SetBranchAddress("_truth_eta", &_truth_eta);
-    tree_chain->SetBranchAddress("_truth_e", &_truth_e);
+    chain->SetBranchAddress("_emcal_phi", &_emcal_phi);
+    chain->SetBranchAddress("_truth_phi", &_truth_phi);
+    chain->SetBranchAddress("_truth_eta", &_truth_eta);
+    chain->SetBranchAddress("_truth_e", &_truth_e);
 
     // 获取条目数量
-    Long64_t nentries = tree_chain->GetEntries();
+    Long64_t nentries = chain->GetEntries();
 
     // 按顺序读取每个条目中的vector
     for (Long64_t i = 0; i < nentries; i++) 
     {
-        tree_chain->GetEntry(i);
+        chain->GetEntry(i);
 
         // map 1 - truth; 2 - g4hit; 3 - cluster reco; 4 - g4particle;
         if (_emcal_phi->size() == 1) 
